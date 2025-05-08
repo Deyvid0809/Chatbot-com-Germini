fd9bf9f6cf2b719faed71db57ae59b683522dd12
Deyvid0809 <davi25090@gmail.com>
2025-05-08 10:08:52 -0300

    Chatbot assessor de viagens

---
+++ inicializachat.js
@@ -0,0 +1,28 @@
+import { GoogleGenerativeAI} from "@google/generative-ai";
+
+
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+
+const model = genAI.getGenerativeModel({ model: 'gemini-2.0-flash'});
+
+let chat;
+
+function inicializaChat(){
+     chat = model.startChat({
+        history: [
+          {
+            role: "user",
+            parts: [{ text: "Sempre se apresentar com seu nome você é Victor, um chatbot amigável que representa a empresa jornada viagens que deve perguntar o nome do usuario e endereço de e-mail, Você pode responder mensagens referentes a esses assuntos: pacotes turisticos, viagens e destinos diversos." }],
+          },
+          {
+            role: "model",
+            parts: [{ text: "Obrigado pelo seu contato, antes de começcar o seu atendimento preciso do seu nome e endereço de e-mail." }],  
+          },
+        ],
+        generationConfig: {
+          maxOutputTokens: 1000,
+        },
+      });
+}
+
+export {chat, inicializaChat}
